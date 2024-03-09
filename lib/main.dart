79b3acb006538f271185bd6d3a913b4ba22bc268
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-09 13:26:11 +0300

    Refactor main.dart to remove unnecessary code

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,50 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Firebase Auth ')
-      ),
-      body: const Center(
-        child: Text('Login Page'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Login Page'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                width: 300,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    labelText: 'Email',
+                    hintText: 'Enter your email',
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              SizedBox(
+                width: 300,
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    labelText: 'Password',
+                    hintText: 'Enter your password',
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () 
+                async {
 
+                },
+                child: const Text('Register'),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
