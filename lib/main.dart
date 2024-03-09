e59e2a2c61c2f36c4673b1c4b357b544efe89d81
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-09 13:13:29 +0300

    Update runApp() to use const constructor

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       ),
       body: const Center(
         child: Text('Login Page'),
-        
+
       ),
     );
   }
