89d09e10c7b785f7455d2e0d89020ab356919ae9
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-12 00:50:09 +0300

    Refactor main function to initialize Firebase before running the app

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main()
-{
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
