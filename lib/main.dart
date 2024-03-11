3107c79974cd3d11bbc39c31dd41b6d746980a3e
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-12 00:55:39 +0300

    Delete authentication and firebase options files

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+void main()
+{
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
