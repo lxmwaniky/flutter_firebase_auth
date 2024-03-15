f066a337e983bdf15e55455f080f9424c2e61281
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-15 23:48:29 +0300

    Update splash screen image asset

--- lib/features/app/splash_screen/splash_screen.dart
+++ lib/features/app/splash_screen/splash_screen.dart
@@ -26,7 +26,7 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Image.asset(''),
+        child: Image.asset('assets/images/logo.png'),
       ),
     );
   }
