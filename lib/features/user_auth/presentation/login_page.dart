3d6d507f7fe9a62a789a3dde39bda1ff88044f37
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-15 23:43:30 +0300

    Fix bug in login functionality

--- lib/features/user_auth/presentation/login_page.dart
+++ lib/features/user_auth/presentation/login_page.dart
@@ -5,6 +5,59 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        backgroundColor: Colors.green[200],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[500],
+          title: const Text('FireBase Authentication'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Welcome to FireBase Authentication', 
+                style: TextStyle(
+                  fontSize: 20, fontWeight: FontWeight.bold
+                ),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: 350,
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Enter your Email',
+                    labelText: 'Email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              SizedBox(
+                width: 350,
+                child: TextField(
+                  enableSuggestions: false,
+                  autocorrect: false,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Enter your Password',
+                    labelText: 'Password',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {}, 
+                child: const Text('Login'),
+              )
+            ],
+          )
+          
+        ),
+      );
   }
 }
