96fbda3515943e7a913a02d8aa703ded0029adc7
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-15 23:28:05 +0300

    Add blank lines to SplashScreen

--- lib/features/app/splash_screen/splash_screen.dart
+++ lib/features/app/splash_screen/splash_screen.dart
@@ -8,6 +8,9 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
