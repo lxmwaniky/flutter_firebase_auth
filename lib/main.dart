bb0edc81d12cc7c2875d3b6a2a71a24e36c7e4c4
Alex Mwaniki Nyambura <lekko254@gmail.com>
2024-03-16 01:14:14 +0300

    Add WidgetsFlutterBinding.ensureInitialized() to main() function

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_firebase_auth/features/app/splash_screen/splash_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
